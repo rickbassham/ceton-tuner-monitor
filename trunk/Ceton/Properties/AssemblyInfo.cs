6354aa9c9fb7ecf7e79acee7015ed78a4904a5ad
unknown <rbassham@RICKBASSHAM8CB8.bcs.capsher.com>
2013-03-05 14:43:59 -0600

    Update copyright info.

--- trunk/Ceton/Properties/AssemblyInfo.cs
+++ trunk/Ceton/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Ceton")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("Copyright © Rick Bassham 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
